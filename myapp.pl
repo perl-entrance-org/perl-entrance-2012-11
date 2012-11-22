ecc87656ab95f69e56576886428322303f471db9
nqounet <nqou.net@gmail.com>
2012-11-22 15:29:12 +0900

    長い文が投稿されても、ちゃんと表示できるようにしてみた

--- myapp.pl
+++ myapp.pl
@@ -4,9 +4,14 @@ use Mojolicious::Lite;
 
 get '/' => sub {
   my $self = shift;
+  $self->render('index');
+};
+
+post '/post' => sub {
+  my $self = shift;
   my $body = $self->param('body');
   $self->stash(body => $body);
-  $self->render('index');
+  $self->render('post');
 };
 
 app->start;
@@ -14,8 +19,16 @@ __DATA__
 
 @@ index.html.ep
 % layout 'default';
-% title 'Welcome';
-%= form_for '/' => begin
+% title '入力フォーム';
+%= form_for '/post' => method => 'POST' => begin
+  %= text_field 'body'
+  %= submit_button '投稿する'
+% end
+
+@@ post.html.ep
+% layout 'default';
+% title '出力';
+%= form_for '/post' => method => 'POST' => begin
   %= text_field 'body'
   %= submit_button '投稿する'
 % end
